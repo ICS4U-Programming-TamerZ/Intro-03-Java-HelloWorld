368b96608f3d25ecbe834182f9b4029a24151781
Tamer Zreg <tamer.zreg@stu.ocsb.ca>
2024-02-09 03:34:02 +0000

    Added HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * Prints Hello World.
+ *
+ * @author Tamer
+ * @version 1.0
+ * @since 2024/02/08
+ */
+
+// Class HelloWorld
+public class HelloWorld {
+    // Main method entry point
+    public static void main(String[] arg) {
+        // Output to Console
+        System.out.println("Hello, World");
+    } 
+}
